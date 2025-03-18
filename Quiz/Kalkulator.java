66812e4b4fa34d2a8d12842df0201e19c494a969
Hikari_Sven <rafliramadanix@gmail.com>
2025-03-18 09:22:16 +0700

    kal

--- Quiz/Kalkulator.java
+++ Quiz/Kalkulator.java
@@ -4,21 +4,17 @@ public class Kalkulator {
         return a + b;
     }
 
-    // Metode untuk menambahkan tiga bilangan bulat
     public int add(int a, int b, int c) {
         return a + b + c;
     }
 
-    // Metode untuk menambahkan dua bilangan desimal
     public double add(double d, double e) {
         return d + e;
     }
 
-    // Metode utama untuk pengujian
     public static void main(String[] args) {
         Kalkulator kal = new Kalkulator();
 
-        // Menguji metode add()
         System.out.println("Penjumlahan 2 bilangan bulat: " + kal.add(5, 7));
         System.out.println("Penjumlahan 3 bilangan bulat: " + kal.add(2, 3, 4));
         System.out.println("Penjumlahan 2 bilangan desimal: " + kal.add(2.5, 3.8));
